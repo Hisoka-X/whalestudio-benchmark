63ef7a0da1ad1332e1fd3d63a0a72d84fb846c90
Jia Fan <fanjiaeminem@qq.com>
2023-06-15 16:18:35 +0800

    Add table create and data generate command (#5)

--- whaletunnel-benchmark-cli/src/main/java/io/whaleops/whaletunnel/benchmark/cli/commands/DataGenerateCommand.java
+++ whaletunnel-benchmark-cli/src/main/java/io/whaleops/whaletunnel/benchmark/cli/commands/DataGenerateCommand.java
@@ -1,8 +1,87 @@
 package io.whaleops.whaletunnel.benchmark.cli.commands;
 
+import io.whaleops.whaletunnel.benchmark.cli.configuration.WhaleTunnelBenchmarkConfiguration;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.regex.Pattern;
+
 /**
  * Use to generate data for benchmark.
  */
+@ShellComponent
+@Slf4j
 public class DataGenerateCommand {
+
+    private static final String insertSql = "INSERT INTO %s (smallint_col, integer_col, " +
+        "bigint_col, decimal_col, numeric_col, real_col, double_col, smallserial_col, serial_col, bigserial_col," +
+        " varchar_col, text_col, date_col, time_col, timestamp_col, boolean_col, bytea_col)" +
+        " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+    @ShellMethod(key = "data")
+    public String dataGenerate(String database, String tablePattern, int number) throws SQLException {
+
+        String url = WhaleTunnelBenchmarkConfiguration.getProperty("jdbc.url");
+        String password = WhaleTunnelBenchmarkConfiguration.getProperty("jdbc.password");
+        String user = WhaleTunnelBenchmarkConfiguration.getProperty("jdbc.user");
+        Random random = new Random();
+        List<String> tableNames = new ArrayList<>();
+        try (Connection connection = DriverManager.getConnection(url, user, password);
+             ResultSet resultSet =
+                 connection
+                     .getMetaData()
+                     .getTables(database, database, null, new String[]{"TABLE"})) {
+            Pattern pattern = Pattern.compile(tablePattern);
+            while (resultSet.next()) {
+                String tableName = resultSet.getString("TABLE_NAME");
+                if (pattern.matcher(tableName).matches()) {
+                    tableNames.add(tableName);
+                }
+            }
+            log.info("find table: " + tableNames);
+        }
+
+        try (Connection connection = DriverManager.getConnection(url, user, password)) {
+            for (String tableName : tableNames) {
+                String sql = String.format(insertSql, database + "." + tableName);
+
+                try (PreparedStatement statement = connection.prepareStatement(sql)) {
+                    for (int i = 1; i <= number; i++) {
+                        statement.setShort(1, (short) (i % Short.MAX_VALUE));
+                        statement.setInt(2, i);
+                        statement.setLong(3, random.nextLong());
+                        statement.setBigDecimal(4, new BigDecimal("33333.33"));
+                        statement.setBigDecimal(5, new BigDecimal("44444.44"));
+                        statement.setFloat(6, random.nextFloat());
+                        statement.setDouble(7, random.nextDouble());
+                        statement.setShort(8, (short) (i % Short.MAX_VALUE));
+                        statement.setInt(9, i);
+                        statement.setLong(10, random.nextLong());
+                        statement.setString(11, "varchar_col_" + i);
+                        statement.setString(12, "text_col_" + i);
+                        statement.setDate(13, new java.sql.Date(System.currentTimeMillis()));
+                        statement.setTime(14, new java.sql.Time(System.currentTimeMillis()));
+                        statement.setTimestamp(15, new java.sql.Timestamp(System.currentTimeMillis()));
+                        statement.setBoolean(16, random.nextBoolean());
+                        statement.setBytes(17, new byte[]{1, 2, 3, 4, 5});
+                        statement.addBatch();
+                    }
+                    statement.executeBatch();
+                    log.info("finished table insert: " + tableName);
+                }
+            }
+        }
+        return "success";
+    }
 
 }
--- whaletunnel-benchmark-cli/src/main/java/io/whaleops/whaletunnel/benchmark/cli/commands/TableGenerateCommand.java
+++ whaletunnel-benchmark-cli/src/main/java/io/whaleops/whaletunnel/benchmark/cli/commands/TableGenerateCommand.java
@@ -1,7 +1,45 @@
 package io.whaleops.whaletunnel.benchmark.cli.commands;
 
+import io.whaleops.whaletunnel.benchmark.cli.configuration.WhaleTunnelBenchmarkConfiguration;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellOption;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  * Use to generate table for benchmark.
  */
+@ShellComponent
 public class TableGenerateCommand {
+
+    private String sqlTemplete = "CREATE TABLE %s ( `id` int(11) NOT NULL AUTO_INCREMENT," +
+        " `smallint_col` smallint(6) DEFAULT NULL, `integer_col` int(11) DEFAULT NULL, `bigint_col` bigint(20) DEFAULT NULL," +
+        " `decimal_col` decimal(10,2) DEFAULT NULL, `numeric_col` decimal(10,2) DEFAULT NULL, `real_col` float DEFAULT NULL," +
+        " `double_col` double DEFAULT NULL, `smallserial_col` smallint(6) DEFAULT NULL, `serial_col` int(11) DEFAULT NULL," +
+        " `bigserial_col` bigint(20) DEFAULT NULL, `varchar_col` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL," +
+        " `text_col` text COLLATE utf8mb4_unicode_ci, `date_col` date DEFAULT NULL, `time_col` time DEFAULT NULL," +
+        " `timestamp_col` datetime DEFAULT NULL, `boolean_col` tinyint(1) DEFAULT NULL, `bytea_col` longblob," +
+        " PRIMARY KEY (`id`) ) ENGINE=InnoDB AUTO_INCREMENT=2112257 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;";
+
+    @ShellMethod(key = "table")
+    public String tableGenerate(String database, @ShellOption(defaultValue = "all_types_table_") String prefix, int number) throws SQLException {
+
+        String url = WhaleTunnelBenchmarkConfiguration.getProperty("jdbc.url");
+        String password = WhaleTunnelBenchmarkConfiguration.getProperty("jdbc.password");
+        String user = WhaleTunnelBenchmarkConfiguration.getProperty("jdbc.user");
+
+        try (Connection connection = DriverManager.getConnection(url, user, password)) {
+            Statement statement = connection.createStatement();
+            for (int i = 1; i <= number; i++) {
+                String sql = String.format(sqlTemplete, String.format(database + ".`" + prefix + "%04d`", i));
+                statement.executeUpdate(sql);
+            }
+            return "success with " + number + " tables";
+        }
+    }
+
 }
