d995868a7a01e67e05c8366d1df1d03a691d55ac
Wenjun Ruan <wenjun@apache.org>
2023-06-16 13:46:53 +0800

    Add analysis workflow instance command (#11)

--- whaletunnel-benchmark-cli/src/main/java/io/whaleops/whaletunnel/benchmark/cli/commands/WorkflowInstanceCommand.java
+++ whaletunnel-benchmark-cli/src/main/java/io/whaleops/whaletunnel/benchmark/cli/commands/WorkflowInstanceCommand.java
@@ -2,7 +2,9 @@ package io.whaleops.whaletunnel.benchmark.cli.commands;
 
 import org.apache.commons.collections4.CollectionUtils;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.springframework.shell.standard.ShellComponent;
@@ -17,7 +19,7 @@ import lombok.extern.slf4j.Slf4j;
 @ShellComponent
 public class WorkflowInstanceCommand {
 
-    @ShellMethod(key = "dwfi", value = "Clear workflow instance under given projects, e.g. dwfi -p 1,2,3")
+    @ShellMethod(key = "delete-wfi", value = "Clear workflow instance under given projects, e.g. delete-wfi -p 1,2,3")
     public void clearWorkflowInstance(@ShellOption(value = "p", help = "projectCodes") List<Long> projectCodes) {
         List<WorkflowInstance> workflowInstances = WhaleSchedulerSdk.listWorkflowInstances(projectCodes);
         if (CollectionUtils.isEmpty(workflowInstances)) {
@@ -30,5 +32,24 @@ public class WorkflowInstanceCommand {
             .collect(Collectors.joining(","));
         WhaleSchedulerSdk.deleteWorkflowInstance(workflowInstanceIds);
         log.info("Success delete workflow instances: {} under given project", workflowInstances.size());
+    }
+
+    @ShellMethod(key = "analysis-wfi", value = "Query workflow instance status under given projects, e.g. analysis-wfi -p 1,2,3")
+    public String analysisWorkflowInstance(@ShellOption(value = "p", help = "projectCodes") List<Long> projectCodes) {
+        List<WorkflowInstance> workflowInstances = WhaleSchedulerSdk.listWorkflowInstances(projectCodes);
+        if (CollectionUtils.isEmpty(workflowInstances)) {
+            log.warn("There is no workflow instance under given projects: {}", projectCodes);
+            return "";
+        }
+        Map<String, Long> workflowInstanceMap = new HashMap<>();
+        for (WorkflowInstance workflowInstance : workflowInstances) {
+            workflowInstanceMap.put(workflowInstance.getState(), workflowInstanceMap.getOrDefault(workflowInstance.getState(), 0L) + 1);
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Workflow Instance Status:").append("\t").append("Count").append("\n");
+        for (Map.Entry<String, Long> entry : workflowInstanceMap.entrySet()) {
+            stringBuilder.append(entry.getKey()).append("\t").append(entry.getValue()).append("\n");
+        }
+        return stringBuilder.toString();
     }
 }
